dff359fed4c46b2f547d5e388995465da5fb966d
Donnacha Fallon <S00167922@mail.itsligo.ie>
2018-02-14 12:51:52 +0000

    more progress

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,32 +1,33 @@
-
-
-
-import { Component, OnInit } from '@angular/core';
-import { HostBinding } from '@angular/core/src/metadata/directives';
+import { 
+  Component,
+  OnInit,
+  Input,
+  HostBinding
+} from '@angular/core';
+import {Article} from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
+
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  votes: number;
-  title: string;
-  link: string;
+  @Input() article: Article;
 
   constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+    
    }
 
-   voteUp(){
-     this.votes += 1;
+   voteUp(): boolean {
+     this.article.voteUp();
+     return false;
    }
 
-   voteDown(){
-     this.votes -= 1;
+   voteDown(): boolean {
+     this.article.voteDown();
+     return false;
    }
 
   ngOnInit() {
